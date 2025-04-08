369cfc63c0c09911768dd321d854d0aa5829605d
Ahmed230460 <128981233+Ahmed230460@users.noreply.github.com>
2025-04-08 04:11:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ st.markdown("""
         justify-content: center;
         align-items: center;
         margin-bottom: 10px;
-        margin-left: 50px;  /* إزاحة اللوجو لليمين */
+        margin-left: 100px;  /* إزاحة اللوجو لليمين */
     }
     .center-image img {
         width: 300px;  /* Set a fixed width */
